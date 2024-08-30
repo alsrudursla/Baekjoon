f87f615c91128cc22e7c140cfa09a28ce08595e7
이민경 <90559205+alsrudursla@users.noreply.github.com>
2024-08-30 15:13:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,13 +1,13 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     static int[] dy = {1, -1, 0, 0};
     static int[] dx = {0, 0, 1, -1};
-    static int N;
     static int[][] house;
     static boolean[][] visited;
-    static Queue<int[]> myqueue;
+    static int N;
+    static int house_size;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -15,61 +15,53 @@ public class Main {
 
         house = new int[N][N];
         visited = new boolean[N][N];
-        myqueue = new LinkedList<>();
 
         for (int i = 0; i < N; i++) {
-            char[] house_tmp = br.readLine().toCharArray();
-            for (int j = 0; j < house_tmp.length; j++) {
-                house[i][j] = Integer.parseInt(String.valueOf(house_tmp[j]));
+            char[] tmp = br.readLine().toCharArray();
+            for (int j = 0; j < N; j++) {
+                house[i][j] = Integer.parseInt(String.valueOf(tmp[j]));
             }
         }
 
-        int house_cnt = 0;
-        List<Integer> house_size = new ArrayList<>();
+        int house_num = 0;
+        List<Integer> house_size_list = new ArrayList<>();
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (house[i][j] == 1 && !visited[i][j]) {
                     visited[i][j] = true;
-                    house_cnt++;
-                    house_size.add(bfs(i, j));
+                    house_num++;
+                    house_size = 0;
+                    dfs(i, j);
+                    house_size_list.add(house_size);
                 }
             }
         }
 
-        bw.write(String.valueOf(house_cnt));
+        bw.write(String.valueOf(house_num));
         bw.newLine();
-
-        Collections.sort(house_size);
-        for (int i = 0; i < house_size.size(); i++) {
-            bw.write(String.valueOf(house_size.get(i)));
+        
+        Collections.sort(house_size_list);
+        for (int i = 0; i < house_size_list.size(); i++) {
+            bw.write(String.valueOf(house_size_list.get(i)));
             bw.newLine();
         }
+
         bw.flush();
         bw.close();
     }
 
-    private static int bfs(int i, int j) {
-        int house_size_val = 1;
-        myqueue.offer(new int[]{i,j});
-
-        while(!myqueue.isEmpty()) {
-            int[] now = myqueue.poll();
-            int now_x = now[1];
-            int now_y = now[0];
-
-            for (int k = 0; k < 4; k++) {
-                int x = now_x + dx[k];
-                int y = now_y + dy[k];
-                if (0 <= x && x < N && 0 <= y && y < N) {
-                    if (house[y][x] == 1 && !visited[y][x]) {
-                        visited[y][x] = true;
-                        house_size_val++;
-                        myqueue.offer(new int[]{y,x});
-                    }
+    private static void dfs(int i, int j) {
+        house_size++;
+        for (int k = 0; k < 4; k++) {
+            int x = dx[k] + j;
+            int y = dy[k] + i;
+            if (0 <= x && x < N && 0 <= y && y < N) {
+                if (house[y][x] == 1 && !visited[y][x]) {
+                    visited[y][x] = true;
+                    dfs(y, x);
                 }
             }
         }
-
-        return house_size_val;
     }
 }
