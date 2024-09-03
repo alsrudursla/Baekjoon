910a90850bf47fb574dcbdc736275a294778996a
이민경 <90559205+alsrudursla@users.noreply.github.com>
2024-09-03 20:49:08 +0900

    [Silver III] Title: 2×n 타일링, Time: 100 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,21 +1,26 @@
 import java.io.*;
 
-// 동적 계획법
+// DP
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        long[] arr = new long[1001];
+        int N = Integer.parseInt(br.readLine());
 
-        arr[1] = 1;
-        arr[2] = 2;
+        // 1. N=1 : 2x1 1개
+        // 2. N=2 : 2x1 2개 or 1x2 2개 → 2가지
+        // 3. N=3 : 2x1 3개 or (2x1 1개 & N=2일때경우) → 3가지
+        // 점화식 An = An-1 + An-2
 
-        for (int i = 3; i <= n; i++) {
-            arr[i] = (arr[i-1] + arr[i-2])%10007;
+        int[] square = new int[1001];
+        square[1] = 1;
+        square[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            square[i] = (square[i-1] + square[i-2]) % 10007;
         }
 
-        bw.write(String.valueOf(arr[n]));
+        bw.write(String.valueOf(square[N]));
         bw.newLine();
         bw.flush();
         bw.close();
