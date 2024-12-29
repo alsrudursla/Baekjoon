856f70279e305cece73c0b052d4562346139accb
이민경 <90559205+alsrudursla@users.noreply.github.com>
2024-12-29 16:41:07 +0900

    [level 2] Title: 전화번호 목록, Time: 294.38 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,18 +1,19 @@
-/*
-- 숫자 문자열 정렬 : 앞에서부터 하나씩 비교 ex. 10 보다 2 가 뒤에 온다
-- 정렬한 후, 인접한 두 전화번호만 비교해도 접두사인지 쉽게 확인할 수 있습니다. 왜냐하면, 문자열 배열을 정렬하면 접두사가 있는 경우, 접두사 관계에 있는 전화번호가 인접하게 배치되기 때문입니다.
-*/
-
+import java.io.*;
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
+        // phone_book : 전화번호 배열 (중복 X)
+        // 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true
         boolean answer = true;
         
-        Arrays.sort(phone_book); 
+        // 정렬
+        Arrays.sort(phone_book);
         
-        for (int t = 0; t < phone_book.length - 1; t++) {
-            // 앞 번호가 뒤 번호의 접두사인지 확인
-            if (phone_book[t+1].startsWith(phone_book[t])) answer = false;
+        // 앞 뒤로 비교
+        for (int i = 0; i < phone_book.length-1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                answer = false;
+            }
         }
         
         return answer;
