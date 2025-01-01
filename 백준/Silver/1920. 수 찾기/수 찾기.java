15663ccfef574062032e1dffd40e0854175a694e
이민경 <90559205+alsrudursla@users.noreply.github.com>
2025-01-01 17:45:53 +0900

    [Silver IV] Title: 수 찾기, Time: 652 ms, Memory: 52648 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,43 +1,49 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 이진 탐색
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
 
-        int[] A = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        long[] nums = new long[N];
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            nums[i] = Long.parseLong(st.nextToken());
         }
-        Arrays.sort(A);
+        Arrays.sort(nums);
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int j = 0; j < M; j++) {
-            int num = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < M; i++) {
+            long number = Long.parseLong(st.nextToken());
 
+            boolean chk = false;
             int start_idx = 0;
-            int end_idx = A.length - 1;
-            boolean find = false;
-            while (start_idx <= end_idx) {
-                int mid_idx = (start_idx + end_idx)/2;
-                int mid_val = A[mid_idx];
-                if (mid_val > num) {
-                    end_idx = mid_idx - 1;
-                } else if (mid_val < num) {
-                    start_idx = mid_idx + 1;
-                } else {
-                    find = true;
+            int end_idx = N-1;
+            while(start_idx <= end_idx) {
+                int mid = (start_idx + end_idx) / 2;
+                if (number == nums[mid]) {
+                    chk = true;
+                    bw.write(String.valueOf(1));
+                    bw.newLine();
                     break;
+                } else if (number > nums[mid]) {
+                    start_idx = mid + 1;
+                } else {
+                    end_idx = mid - 1;
                 }
             }
 
-            if (find) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
+            if (!chk) {
+                bw.write(String.valueOf(0));
+                bw.newLine();
             }
         }
+        bw.flush();
+        bw.close();
     }
 }
