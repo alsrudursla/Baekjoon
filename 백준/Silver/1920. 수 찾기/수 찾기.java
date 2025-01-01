9b92a812122094a876c6f8eba86d57d4d789615b
이민경 <90559205+alsrudursla@users.noreply.github.com>
2025-01-01 17:53:36 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 45932 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,49 +1,45 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
-// 이진 탐색
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static int[] A;
+    public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int N = Integer.parseInt(br.readLine());
+        A = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long[] nums = new long[N];
         for (int i = 0; i < N; i++) {
-            nums[i] = Long.parseLong(st.nextToken());
+            int input = Integer.parseInt(st.nextToken());
+            A[i] = input;
         }
-        Arrays.sort(nums);
+        Arrays.sort(A);
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            long number = Long.parseLong(st.nextToken());
-
-            boolean chk = false;
-            int start_idx = 0;
-            int end_idx = N-1;
-            while(start_idx <= end_idx) {
-                int mid = (start_idx + end_idx) / 2;
-                if (number == nums[mid]) {
-                    chk = true;
-                    bw.write(String.valueOf(1));
-                    bw.newLine();
-                    break;
-                } else if (number > nums[mid]) {
-                    start_idx = mid + 1;
-                } else {
-                    end_idx = mid - 1;
-                }
-            }
-
-            if (!chk) {
-                bw.write(String.valueOf(0));
-                bw.newLine();
-            }
+            int num = Integer.parseInt(st.nextToken());
+            int ans = findNumber(num, 0, N-1);
+            bw.write(String.valueOf(ans));
+            bw.newLine();
         }
         bw.flush();
         bw.close();
+    }
+
+    private static int findNumber(int target, int start, int end) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (A[mid] == target) {
+                return 1;
+            } else if (A[mid] < target) {
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return 0;
     }
 }
