9da7f09859d9bb04c2b2feaeb5c5bcedda3e23b0
alsrudursla <90559205+alsrudursla@users.noreply.github.com>
2025-06-01 18:25:22 +0900

    [Silver I] Title: 쿼드트리, Time: 188 ms, Memory: 16792 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -16,13 +16,10 @@ public class Main {
             }
         }
 
-        if (N==1) {
-            bw.write(String.valueOf(map[0][0]));
-        } else {
-            // 4분할
-            String ans = compress(N, 0, 0);
-            bw.write(ans);
-        }
+        // 4분할
+        String ans = compress(N, 0, 0);
+        bw.write(ans);
+
         bw.flush();
         bw.close();
     }
@@ -33,39 +30,21 @@ public class Main {
         else if (isAll1(startY, startX, startY + size - 1, startX + size - 1)) return "1";
 
         // 1. 왼쪽 위 검사
-        String chk1 = chkLeftUp(startY, startX, startY + size / 2 - 1, startX + size / 2 - 1, size);
+        String chkLeftUp = chk(startY, startX, startY + size / 2 - 1, startX + size / 2 - 1, size);
 
         // 2. 오른쪽 위 검사
-        String chk2 = chkRightUp(startY, startX + size / 2, startY + size / 2 - 1, startX + size - 1, size);
+        String chkRightUp = chk(startY, startX + size / 2, startY + size / 2 - 1, startX + size - 1, size);
 
         // 3. 왼쪽 아래 검사
-        String chk3 = chkLeftDown(startY + size / 2, startX, startY + size - 1, startX + size / 2 - 1, size);
+        String chkLeftDown = chk(startY + size / 2, startX, startY + size - 1, startX + size / 2 - 1, size);
 
         // 4. 오른쪽 아래 검사
-        String chk4 = chkRightDown(startY + size / 2, startX + size / 2, startY + size - 1, startX + size - 1, size);
+        String chkRightDown = chk(startY + size / 2, startX + size / 2, startY + size - 1, startX + size - 1, size);
 
-        return "(" + chk1 + chk2 + chk3 + chk4 + ")";
+        return "(" + chkLeftUp + chkRightUp + chkLeftDown + chkRightDown + ")";
     }
 
-    private static String chkLeftUp(int startY, int startX, int endY, int endX, int size) {
-        if (isAll0(startY, startX, endY, endX)) return "0";
-        else if (isAll1(startY, startX, endY, endX)) return "1";
-        else return compress(size/2, startY, startX);
-    }
-
-    private static String chkLeftDown(int startY, int startX, int endY, int endX, int size) {
-        if (isAll0(startY, startX, endY, endX)) return "0";
-        else if (isAll1(startY, startX, endY, endX)) return "1";
-        else return compress(size/2, startY, startX);
-    }
-
-    private static String chkRightUp(int startY, int startX, int endY, int endX, int size) {
-        if (isAll0(startY, startX, endY, endX)) return "0";
-        else if (isAll1(startY, startX, endY, endX)) return "1";
-        else return compress(size/2, startY, startX);
-    }
-
-    private static String chkRightDown(int startY, int startX, int endY, int endX, int size) {
+    private static String chk(int startY, int startX, int endY, int endX, int size) {
         if (isAll0(startY, startX, endY, endX)) return "0";
         else if (isAll1(startY, startX, endY, endX)) return "1";
         else return compress(size/2, startY, startX);
