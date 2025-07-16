8e29945e4f2ca30ad20d0050a6087420d88ceec6
alsrudursla <90559205+alsrudursla@users.noreply.github.com>
2025-07-16 18:06:43 +0900

    [Silver IV] Title: 수 찾기, Time: 504 ms, Memory: 48352 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,45 +1,28 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    static int[] A;
-    public static void main (String[] args) throws IOException {
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        A = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+        Set<Integer> A = new HashSet<>(); // 주어진 배열
         for (int i = 0; i < N; i++) {
-            int input = Integer.parseInt(st.nextToken());
-            A[i] = input;
+            A.add(Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(A);
 
         int M = Integer.parseInt(br.readLine());
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
             int num = Integer.parseInt(st.nextToken());
-            int ans = findNumber(num, 0, N-1);
-            bw.write(String.valueOf(ans));
-            bw.newLine();
+            if (A.contains(num)) bw.write("1\n");
+            else bw.write("0\n");
         }
         bw.flush();
         bw.close();
-    }
-
-    private static int findNumber(int target, int start, int end) {
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (A[mid] == target) {
-                return 1;
-            } else if (A[mid] < target) {
-                start = mid+1;
-            } else {
-                end = mid-1;
-            }
-        }
-        return 0;
     }
 }
