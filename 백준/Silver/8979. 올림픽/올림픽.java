b3597ae1e31f58fbc15b4cf0604bd7b1c2b10806
alsrudursla <90559205+alsrudursla@users.noreply.github.com>
2025-09-19 02:28:41 +0900

    [Silver V] Title: 올림픽, Time: 124 ms, Memory: 14752 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -27,6 +27,7 @@ class Main {
         int beforeG = -1;
         int beforeS = -1;
         int beforeB = -1;
+        int cnt = 0;
         int rank = 0;
         while (!pq.isEmpty()) {
             int[] now = pq.poll();
@@ -35,34 +36,19 @@ class Main {
             int silver = now[2];
             int bronze = now[3];
 
-            // 첫 번째 원소일 때
-            if (beforeG == -1) {
+            // 셋 다 같을 때만 동일 순위 (rank 유지)
+            if (beforeG == gold && beforeS == silver && beforeB == bronze) {
+                // 동일 등수 카운트
+                cnt++;
+            } else {
+                rank += cnt;
+                cnt = 0;
+                
                 beforeG = gold;
                 beforeS = silver;
                 beforeB = bronze;
                 rank++;
-            } else { // 그 외
-                if (beforeG == gold) {
-                    if (beforeS == silver) {
-                        // 모두 동일하면 rank 도 동일, 그대로 넘어감
-                        if (beforeB != bronze) {
-                            beforeB = bronze;
-                            rank++;
-                        }
-                    } else {
-                        beforeS = silver;
-                        beforeB = bronze;
-                        rank++;
-                    }
-                } else {
-                    beforeG = gold;
-                    beforeS = silver;
-                    beforeB = bronze;
-                    rank++;
-                }
             }
-
-            ///System.out.println(cid + " " + rank);
 
             if (cid == target) {
                 chk = true;
