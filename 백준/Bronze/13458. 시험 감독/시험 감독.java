ff2fae5435728f9d1f3e10b7a42f96465a1ca463
alsrudursla <90559205+alsrudursla@users.noreply.github.com>
2025-10-17 16:36:35 +0900

    [Bronze II] Title: 시험 감독, Time: 440 ms, Memory: 92460 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.java
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -1,44 +1,39 @@
-import java.io.*;
 import java.util.*;
-public class Main {
+import java.io.*;
+
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine()); // 시험장 개수
-
-        int[] students = new int[N]; // 시험장 별 응시자 수
+        int spaceNum = Integer.parseInt(br.readLine());
+        
+        int[] peopleNum = new int[spaceNum];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            students[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < spaceNum; i++) {
+            peopleNum[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
-        int B = Integer.parseInt(st.nextToken()); // 총감독관 감시 응시자 수
-        int C = Integer.parseInt(st.nextToken()); // 부감독관 감시 응시자 수
-        long ans = 0; // 필요한 감독관 수의 최솟값
-        // 각각의 시험장에 총감독관은 오직 1명만 있어야 하고, 부감독관은 여러 명 있어도 된다.
+        int mainCnt = Integer.parseInt(st.nextToken());
+        int subCnt = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            int student = students[i];
-            student -= B; // 총감독관 감시
-            ans++;
+        long answer = 0L;
+        for (int i = 0; i < spaceNum; i++) {
+            int rest = peopleNum[i] - mainCnt;
+            answer++;
+            
+            if (rest <= 0) continue; // 총감독관으로 충분
+            else {
+                int quo = rest / subCnt;
+                int remain = rest % subCnt;
 
-            int supervisor = 0; // 필요한 부감독관
-            if (student > 0) {
-                if (student <= C) supervisor = 1;
-                else {
-                    if (student % C == 0) {
-                        supervisor = student / C;
-                    } else {
-                        supervisor = student / C + 1;
-                    }
-                }
+                if (remain != 0) {
+                    answer += (quo + 1);
+                } else answer += quo;
             }
-            ans += supervisor;
         }
 
-        bw.write(String.valueOf(ans));
-        bw.newLine();
+        bw.write(String.valueOf(answer));
         bw.flush();
         bw.close();
     }
