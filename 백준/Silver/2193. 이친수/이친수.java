8c7bfaf64072e69c289e135927ee1e0db526c27d
alsrudursla <90559205+alsrudursla@users.noreply.github.com>
2025-11-07 15:18:17 +0900

    [Silver III] Title: 이친수, Time: 172 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        long[] dp = new long[91];
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 3; i <= 90; i++) {
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+
+        System.out.println(dp[N]);
+        sc.close();
+    }
+}
