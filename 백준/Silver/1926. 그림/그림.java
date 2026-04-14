e6e9ec9d1249301630a00c7a045d4aee0b5a0100
alsrudursla <90559205+alsrudursla@users.noreply.github.com>
2026-04-14 18:18:36 +0900

    [Silver I] Title: 그림, Time: 404 ms, Memory: 53008 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,76 +1,71 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-// BFS
-public class Main {
-    static int[] dy = {1, -1, 0, 0};
-    static int[] dx = {0, 0, 1, -1};
-    static int N, M;
-    static int[][] img;
+class Main {
+    static int height, width;
+    static int[][] map;
     static boolean[][] visited;
-    static Queue<int[]> myqueue;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 세로
-        M = Integer.parseInt(st.nextToken()); // 가로
-
-        img = new int[N][M];
-        visited = new boolean[N][M];
-        myqueue = new LinkedList<>();
-
-        // 그림 채우기
-        for (int i = 0; i < N; i++) {
+        height = Integer.parseInt(st.nextToken());
+        width = Integer.parseInt(st.nextToken());
+        
+        map = new int[height][width];
+        for (int i = 0; i < height; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                img[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < width; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int img_num = 0;
-        int img_max_size = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (img[i][j] == 1 && !visited[i][j]) {
-                    visited[i][j] = true;
-                    img_num++;
-                    int img_size = bfs(i, j);
-                    if (img_size > img_max_size) {
-                        img_max_size = img_size;
-                    }
-                }
+        visited = new boolean[height][width];
+        int picCnt = 0;
+        int picMax = 0;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (visited[i][j] || map[i][j] == 0) continue;
+                visited[i][j] = true;
+                picCnt++;
+                int size = findSize(i, j);
+                picMax = Math.max(picMax, size);
             }
         }
 
-        bw.write(String.valueOf(img_num));
-        bw.newLine();
-        bw.write(String.valueOf(img_max_size));
-        bw.newLine();
+        bw.write(String.valueOf(picCnt) + '\n');
+        bw.write(String.valueOf(picMax));
         bw.flush();
         bw.close();
     }
 
-    private static int bfs(int i, int j) {
-        int now_img_size = 1;
-        myqueue.offer(new int[]{i,j});
+    private static int findSize(int i, int j) {
+        int[] dy = {-1, 0, 1, 0};
+        int[] dx = {0, 1, 0, -1};
+        
+        Queue<int[]> myqueue = new LinkedList<>();
+        myqueue.add(new int[]{i, j});
+
+        int size = 1;
         while (!myqueue.isEmpty()) {
             int[] now = myqueue.poll();
-            int now_y = now[0];
-            int now_x = now[1];
+            int nowY = now[0];
+            int nowX = now[1];
 
             for (int k = 0; k < 4; k++) {
-                int y = now_y + dy[k];
-                int x = now_x + dx[k];
-                if (0 <= x && x < M && 0 <= y && y < N) {
-                    if (img[y][x] == 1 && !visited[y][x]) {
-                        visited[y][x] = true;
-                        now_img_size++;
-                        myqueue.offer(new int[]{y,x});
+                int nextY = nowY + dy[k];
+                int nextX = nowX + dx[k];
+
+                if (0 <= nextY && 0 <= nextX && nextY < height && nextX < width) {
+                    if (!visited[nextY][nextX] && map[nextY][nextX] == 1) {
+                        visited[nextY][nextX] = true;
+                        size++;
+                        myqueue.add(new int[]{nextY, nextX});
                     }
                 }
             }
         }
-        return now_img_size;
+
+        return size;
     }
 }
